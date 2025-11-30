098d66f247148c9fe96fd82bc35f9a6d1ebc5aec
Wirmaple73 <wirmaple73@gmail.com>
2025-11-30 07:58:37 +0330

    Add 'file path' to DuplicateFileException and FileNotFoundException.

--- Bloat/Exceptions.h
+++ Bloat/Exceptions.h
@@ -5,11 +5,32 @@
 class InvalidArchiveException : public std::runtime_error
 {
 public:
-    inline explicit InvalidArchiveException(const std::string& message) : std::runtime_error(message) {}
+    inline explicit InvalidArchiveException(const std::string& message) noexcept : std::runtime_error(message) {}
 };
 
 class DuplicateFileException : public std::runtime_error
 {
+private:
+    fs::path filePath;
+
 public:
-    inline explicit DuplicateFileException(const std::string& message) : std::runtime_error(message) {}
+    inline explicit DuplicateFileException(const fs::path& filePath) noexcept
+        : std::runtime_error("Another file with the same name already exists in the specified path."), filePath(filePath) {}
+
+    inline explicit DuplicateFileException(const std::string& message, const fs::path& filePath) noexcept
+        : std::runtime_error(message), filePath(filePath) {}
+
+    const fs::path& GetFilePath() const noexcept { return filePath; }
+};
+
+class FileNotFoundException : public std::runtime_error
+{
+private:
+    fs::path filePath;
+
+public:
+    inline explicit FileNotFoundException(const std::string& message, const fs::path& filePath) noexcept
+        : std::runtime_error(message), filePath(filePath) {}
+
+    const fs::path& GetFilePath() const noexcept { return filePath; }
 };
