40f20cb556210f018397e8c2b88ac84f5798bbe6
Wirmaple73 <wirmaple73@gmail.com>
2025-11-30 07:57:31 +0330

    Add major overhauls.

--- Bloat/BloatArchive.cpp
+++ Bloat/BloatArchive.cpp
@@ -1,43 +1,126 @@
+#pragma once
+#include <execution>
 #include "BloatArchive.h"
-#include "Bloater.h"
-#include "Checksum.h"
 #include "Exceptions.h"
+#include "Obfuscator.h"
 #include "Stream.h"
-#include "XorshiftRandom.h"
+#include "StringUtils.h"
+#include "SplitMix64.h"
+#include "Xorshift64Star.h"
+
+// Private methods
+
+size_t BloatArchive::GetActiveFileCount() const noexcept
+{
+	auto view = files | std::views::filter([](const ArchiveFile& file) { return !file.IsRemoved(); });
+	return std::ranges::distance(view);
+}
+
+void BloatArchive::ThrowIfFileDoesNotExist(const fs::path& filePath) const
+{
+	if (!DoesFileExist(filePath))
+		throw FileNotFoundException("The specified file does not exist in the archive.", filePath);
+}
+
+ArchiveFile& BloatArchive::GetFileOrThrow(const fs::path& filePath)
+{
+	ThrowIfFileDoesNotExist(filePath);
+	return files[fileIndices.at(filePath)];
+}
+
+void BloatArchive::ExtractFile(const ArchiveFile& file, const fs::path& destDir,
+	const bool overwriteExisting, const bool throwIfRemoved, const bool throwIfDuplicated) const
+{
+	if (file.IsRemoved())
+	{
+		if (throwIfRemoved)
+			throw FileNotFoundException("The specified file is marked for removal.", file.GetPath());
+
+		return;
+	}
+
+	const fs::path& outputFilePath = destDir / file.GetPath();
+	const fs::path& outputFileParentPath = outputFilePath.parent_path();
+
+	if (!overwriteExisting && fs::is_regular_file(outputFilePath))
+	{
+		if (throwIfDuplicated)
+			throw DuplicateFileException(outputFilePath);
+
+		return;
+	}
+
+	if (!fs::exists(outputFileParentPath))
+		fs::create_directories(outputFileParentPath);
+
+	FileStream::OpenWrite(outputFilePath, true).Write(file.GetBytes());
+}
+
+// Public methods
+
+BloatArchive::BloatArchive() noexcept
+	: scrambler(Scrambler::Create(1ui64, ObfuscatorFactory::Create(ObfuscatorId::RandomXorObfuscator))) { }
 
 uint8_t BloatArchive::GetVersion() const noexcept { return version; }
 
-std::shared_ptr<Scrambler> BloatArchive::GetScrambler() const noexcept { return scrambler; }
+uint64_t BloatArchive::GetScrambledSize() const
+{
+	uint64_t size = 0ui64;
+
+	for (const ArchiveFile& file : files)
+		size += file.GetScrambledSize();
+
+	return size;
+}
+
+uint64_t BloatArchive::GetUnscrambledSize() const
+{
+	uint64_t size = 0ui64;
+
+	for (const ArchiveFile& file : files)
+		size += file.GetUnscrambledSize();
+
+	return size;
+}
+
+const std::shared_ptr<Scrambler>& BloatArchive::GetScrambler() const noexcept { return scrambler; }
 void BloatArchive::SetScrambler(const std::shared_ptr<Scrambler>& scrambler) noexcept { this->scrambler = scrambler; }
 
-std::vector<ArchiveFile> BloatArchive::GetFiles() const noexcept { return files; }
-
-uint64_t BloatArchive::GetChecksum()
+uint64_t BloatArchive::GetChecksum() const
 {
-	// Silly made-up hash accumulator function or something.
-	// I have no idea whether it's decent or utter trash, but we'll call it the glorious BLOATSUM (tm).
+	if (isChecksumUpToDate)
+		return checksum;
 
-	uint64_t checksum = 1610612741ull;
-	uint64_t someWeirdPrime = 50331653ull;
+	// TODO: Bypass the CRT's dumb 512 (or 2048) file limit and parallelize
 
-	for (ArchiveFile& file : files)
+	//std::vector<uint64_t> hashes(GetActiveFileCount());
+	//std::vector<uint64_t> indices(numFiles);
+	//std::iota(indices.begin(), indices.end(), 0);  // Fill indices with 0, 1, 2, ..., numFiles - 1
+	
+	//std::for_each(std::execution::par_unseq, indices.begin(), indices.end(), [this, &hashes](const uint64_t i)
+	//{
+	//	hashes[i] = SplitMix64::ComputeHash(files[i].GetBytes());
+	//});
+
+	uint64_t acc = 0xcbf29ce484222325ui64;  // FNV offset basis number - should be a good starting value
+
+	for (const ArchiveFile& file : files)
 	{
-		const uint64_t fileChecksum = Checksum::Calculate(file.GetBytes());
-
-		checksum ^= fileChecksum;
-		checksum *= (402653189ull * someWeirdPrime) | 1;
-
-		someWeirdPrime ^= checksum;
+		if (!file.IsRemoved())
+			acc ^= SplitMix64::Mix(acc ^ SplitMix64::ComputeHash(file.GetBytes()));
 	}
+
+	checksum = acc;
+	isChecksumUpToDate = true;
 
 	return checksum;
 }
 
-BloatArchive::BloatArchive() noexcept
-	: scrambler(std::make_shared<Scrambler>(1ui64, ObfuscatorFactory::Create(ObfuscatorId::RandomXorObfuscator))) { }
-
 BloatArchive BloatArchive::Open(const fs::path& archivePath)
 {
+	if (!fs::is_regular_file(archivePath))
+		throw std::invalid_argument("The specified path does not represent a BLOAT archive.");
+
 	auto fs = std::make_shared<FileStream>(FileStream::OpenRead(archivePath));
 
 	if (fs->ReadString(MAGIC_NUMBER.length()) != MAGIC_NUMBER)
@@ -47,12 +130,12 @@ BloatArchive BloatArchive::Open(const fs::path& archivePath)
 	archive.version = fs->Read<uint8_t>();
 
 	if (archive.version != CURRENT_ARCHIVE_VERSION)
-		throw InvalidArchiveException("The archive version is not supported by this version of BLOAT CLI.");
+		throw InvalidArchiveException("The archive version is unsupported.");
 
 	const uint64_t bloatMultiplier = fs->Read<uint64_t>();
 	const auto obfuscator = ObfuscatorFactory::Create(static_cast<ObfuscatorId>(fs->Read<uint8_t>()));
 
-	const uint32_t key = fs->Read<uint32_t>();
+	const uint64_t key = fs->Read<uint64_t>();
 
 	if (obfuscator->SupportsKey())
 		obfuscator->SetKey(key);
@@ -63,17 +146,18 @@ BloatArchive BloatArchive::Open(const fs::path& archivePath)
 	const uint64_t numFiles = fs->Read<uint64_t>();
 
 	archive.files.reserve(numFiles);
-	archive.filePaths.reserve(numFiles);
+	archive.fileIndices.reserve(numFiles);
 	
 	for (uint64_t i = 0; i < numFiles; i++)
 	{
 		const uint64_t pathLength = fs->Read<uint64_t>();
-		const std::string path = fs->ReadString(pathLength);
+		const fs::path path(fs->ReadString(pathLength));
 
 		const uint64_t byteLength = fs->Read<uint64_t>();
 
-		archive.files.emplace_back(ArchiveFile(path, fs, archive.scrambler, fs->GetReadPosition(), byteLength));
-		archive.filePaths.insert(path);
+		// TODO: Trim redundant bytes
+		archive.files.emplace_back(ArchiveFile(path, archive.scrambler, archivePath, fs->GetReadPosition(), byteLength));
+		archive.fileIndices[path] = archive.files.size() - 1;
 
 		fs->SetReadPosition(static_cast<uint64_t>(fs->GetReadPosition()) + byteLength);  // Skip to the next file
 	}
@@ -84,101 +168,191 @@ BloatArchive BloatArchive::Open(const fs::path& archivePath)
 	return archive;
 }
 
-void BloatArchive::AddFile(const fs::path& filePath, const fs::path& basePath)
+void BloatArchive::AddFile(const fs::path& filePath, const fs::path& relativePath, const bool overwriteExisting)
 {
-	const fs::path relativePath = fs::relative(filePath, basePath);
+	if (DoesFileExist(relativePath))
+	{
+		if (overwriteExisting)
+			RemoveFile(relativePath);
+		else
+			throw DuplicateFileException("Another file with the same name already exists in the archive.", filePath);
+	}
 
-	if (filePaths.contains(filePath.string()))
-		throw DuplicateFileException(std::format("Another file named \"{}\" already exists in the archive.", relativePath.string()));
+	files.emplace_back(ArchiveFile(filePath, relativePath, scrambler));
+	fileIndices[relativePath] = files.size() - 1;
 
-	auto fs = std::make_shared<FileStream>(FileStream::OpenRead(filePath));
-	files.emplace_back(ArchiveFile(relativePath, fs, scrambler));
-
-	filePaths.insert(filePath.string());
+	isChecksumUpToDate = false;
 }
 
-void BloatArchive::AddFilesFromDirectory(const fs::path& dirPath, const bool recursive)
+void BloatArchive::AddFile(const fs::path& filePath, const bool overwriteExisting)
 {
-	if (!fs::exists(dirPath) || fs::is_directory(dirPath))
-		throw std::invalid_argument("The specified path does not exist.");
+	AddFile(filePath, filePath.filename(), overwriteExisting);
+}
 
+void BloatArchive::AddDirectory(const fs::path& dirPath, const bool recursive, const bool overwriteExisting)
+{
+	if (!fs::is_directory(dirPath))
+		throw std::invalid_argument("The specified path does not exist or is not a valid directory.");
+
+	AggregateException exceptions{};
+
+	const auto& addFile = [this, &exceptions, &dirPath, overwriteExisting](const fs::directory_entry& entry) -> void
+	{
+		try
+		{
+			if (entry.is_regular_file())
+				AddFile(entry.path(), fs::relative(entry.path(), dirPath), overwriteExisting);
+		}
+		catch (...)
+		{
+			exceptions.Add(std::current_exception());
+		}
+	};
+	
 	if (recursive)
 	{
 		for (const auto& file : fs::recursive_directory_iterator(dirPath, fs::directory_options::skip_permission_denied))
-		{
-			if (file.is_regular_file())
-				AddFile(file.path(), dirPath);
-		}
-
-		return;
-	}
-
-	for (const auto& file : fs::directory_iterator(dirPath, fs::directory_options::skip_permission_denied))
-	{
-		// Exclude subdirectories (even top-level ones)
-
-		if (file.is_regular_file())
-			AddFile(file.path(), dirPath);
-	}
-}
-
-void BloatArchive::Extract(const fs::path& destDir, const bool overwriteExistingFiles)
-{
-	for (ArchiveFile& file : files)
-	{
-		const fs::path outputFilePath = destDir / file.GetPath();
-		const fs::path outputFileParentPath = outputFilePath.parent_path();
-
-		if (!overwriteExistingFiles && fs::exists(outputFilePath) && fs::is_regular_file(outputFilePath))
-			continue;
-
-		if (!fs::exists(outputFileParentPath))
-			fs::create_directories(outputFileParentPath);
-
-		FileStream fs = FileStream::OpenWrite(outputFilePath, true);
-		fs.Write(file.GetBytes());
-	}
-}
-
-void BloatArchive::Save(const fs::path& destPath)
-{
-	// A regular FileStream (or std::ofstream) would require truncating the file,
-	// making writing files impossible and risking archive corruption if any exceptions are thrown.
-	MemoryStream ms{};
-	
-	// Write the header
-	ms.Write(std::string{ MAGIC_NUMBER });                                         // Magic number       (offset 0)
-	ms.Write<uint8_t>(CURRENT_ARCHIVE_VERSION);                                    // Archive version: 1 (offset 6)
-	ms.Write<uint64_t>(scrambler->GetBloatMultiplier());                           // Bloat multiplier   (offset 7)
-	ms.Write<uint8_t>(static_cast<uint8_t>(scrambler->GetObfuscator()->GetId()));  // Obfuscator ID		 (offset F)
-
-	if (scrambler->GetObfuscator()->SupportsKey())
-	{
-		scrambler->GetObfuscator()->SetKey(XorshiftRandom().GetSeed());
-		ms.Write<uint32_t>(scrambler->GetObfuscator()->GetKey());                  // Obfuscator key     (offset 10)
+			addFile(file);
 	}
 	else
 	{
-		ms.Write<uint32_t>(0u);
+		for (const auto& file : fs::directory_iterator(dirPath, fs::directory_options::skip_permission_denied))
+			addFile(file);
 	}
 
-	ms.Write<uint64_t>(GetChecksum());                                             // Archive checksum   (offset 14)
-	ms.Write<uint64_t>(uint64_t{ files.size() });                                  // Archive file count (offset 1C)
+	exceptions.ThrowIfNonempty();
+}
 
-	// Write all files to the archive
+void BloatArchive::RemoveFile(const fs::path& filePath)
+{
+	GetFileOrThrow(filePath).MarkAsRemoved();
+	isChecksumUpToDate = false;
+}
+
+void BloatArchive::RemoveDirectory(const fs::path& dirPath)
+{
+	auto normalizedDir = StringUtils::ToLower(dirPath.generic_u8string());
+
+	if (!normalizedDir.ends_with('/'))
+		normalizedDir += '/';
+
 	for (ArchiveFile& file : files)
 	{
-		std::vector<unsigned char> bytes = file.GetBytes();
-		scrambler->Scramble(bytes);
+		if (StringUtils::ToLower(file.GetPath().generic_u8string()).starts_with(normalizedDir))
+		{
+			file.MarkAsRemoved();
+			isChecksumUpToDate = false;
+		}
+	}
+}
 
-		const std::string path = file.GetPath().string();
+const std::vector<ArchiveFile>& BloatArchive::GetAllFiles() const noexcept { return files; }
 
-		ms.Write(uint64_t{ path.length() });
-		ms.Write(path);
+const ArchiveFile& BloatArchive::GetFile(const fs::path& filePath) const
+{
+	ThrowIfFileDoesNotExist(filePath);
+	return files[fileIndices.at(filePath)];
+}
 
-		ms.Write(uint64_t{ bytes.size() });
-		ms.Write(bytes);
+bool BloatArchive::DoesFileExist(const fs::path& filePath) const noexcept
+{
+	return fileIndices.contains(filePath) && !files[fileIndices.at(filePath)].IsRemoved();
+}
+
+void BloatArchive::Extract(const fs::path& destDir, const bool overwriteExistingFiles) const
+{
+	AggregateException exceptions{};
+
+	for (const ArchiveFile& file : files)
+	{
+		try
+		{
+			ExtractFile(file, destDir, overwriteExistingFiles, false, true);
+		}
+		catch (...)
+		{
+			exceptions.Add(std::current_exception());
+		}
 	}
 
-	ms.WriteToFile(destPath);
+	exceptions.ThrowIfNonempty();
+}
+
+void BloatArchive::Extract(const fs::path& filePath, const fs::path& destDir, const bool overwriteExisting, const bool throwIfDuplicated) const
+{
+	ExtractFile(GetFile(filePath), destDir, overwriteExisting, true, throwIfDuplicated);
+}
+
+void BloatArchive::Save(const fs::path& destPath, const bool overwrite) const
+{
+	if (files.empty())
+		throw InvalidArchiveException("The archive must contain at least one file.");
+
+	fs::path tempPath = destPath;
+	tempPath += ".tmp";
+
+	if (!overwrite && (fs::is_regular_file(destPath) || fs::is_regular_file(tempPath)))
+		throw DuplicateFileException(destPath);
+
+	FileStream ts = FileStream::OpenWrite(tempPath, true);
+
+	try
+	{
+		// Write the header
+		ts.Write(std::string{ MAGIC_NUMBER });                                         // Magic number       (offset 0x0)
+		ts.Write<uint8_t>(CURRENT_ARCHIVE_VERSION);                                    // Archive version: 1 (offset 0x7)
+		ts.Write<uint64_t>(scrambler->GetBloatMultiplier());                           // Bloat multiplier   (offset 0x8)
+		ts.Write<uint8_t>(static_cast<uint8_t>(scrambler->GetObfuscator()->GetId()));  // Obfuscator ID		 (offset 0x10)
+
+		if (scrambler->GetObfuscator()->SupportsKey())
+			ts.Write<uint64_t>(scrambler->GetObfuscator()->GetKey());                  // Obfuscator key     (offset 0x11)
+		else
+			ts.Write<uint64_t>(0ui64);
+
+		ts.Write<uint64_t>(GetChecksum());                                             // Archive checksum   (offset 0x19)
+		ts.Write<uint64_t>(uint64_t{ GetActiveFileCount() });                          // Archive file count (offset 0x21)
+
+		// Write all files to the archive
+		for (const ArchiveFile& file : files)
+		{
+			if (file.IsRemoved())
+				continue;
+
+			/* File structure:
+			* Path length (uint64)
+			* Path
+			* Data length (uint64)
+			* Scrambled bytes
+			*/
+
+			std::vector<unsigned char> bytes = file.GetBytes();
+			scrambler->Scramble(bytes);
+
+			const std::u8string path = file.GetPath().generic_u8string();
+
+			ts.Write(static_cast<uint64_t>(path.length()));
+			ts.Write(path);
+
+			ts.Write(static_cast<uint64_t>(bytes.size()));
+			ts.Write(bytes);
+		}
+		
+		ts.Close();
+
+		if (fs::is_regular_file(destPath))
+			fs::remove(destPath);
+
+		fs::rename(tempPath, destPath);
+	}
+	catch (...)
+	{
+		try
+		{
+			ts.Close();
+			fs::remove(tempPath);
+		}
+		catch (...) { /* Swallow to preserve the original exception */ }
+
+		throw;
+	}
 }
--- Bloat/BloatArchive.h
+++ Bloat/BloatArchive.h
@@ -1,8 +1,7 @@
 #pragma once
 #include <filesystem>
-#include <unordered_set>
+#include <unordered_map>
 #include "ArchiveFile.h"
-#include "Obfuscator.h"
 #include "Scrambler.h"
 
 namespace fs = std::filesystem;
@@ -12,30 +11,63 @@ class BloatArchive
 private:
 	uint8_t version = CURRENT_ARCHIVE_VERSION;
 
+	mutable bool isChecksumUpToDate = false;
+	mutable uint64_t checksum = 0ui64;
+
 	std::shared_ptr<Scrambler> scrambler;
 
 	std::vector<ArchiveFile> files{};
-	std::unordered_set<std::string> filePaths{};  // For very fast file duplication checks
+	std::unordered_map<fs::path, size_t> fileIndices{};  // For blazing fast file duplication checks and index lookups
 
-	static inline const std::string MAGIC_NUMBER = "BLTBCS";  // "Bloat Because Compression Sucks"
+	static inline const std::string MAGIC_NUMBER = "\xE9" "BLTBCS";  // "BLOAT Because Compression Sucks"
 	static constexpr inline const uint8_t CURRENT_ARCHIVE_VERSION = 1ui8;
 
+	size_t GetActiveFileCount() const noexcept;
+
+	void ThrowIfFileDoesNotExist(const fs::path& filePath) const;
+	ArchiveFile& GetFileOrThrow(const fs::path& filePath);
+
+	void ExtractFile(const ArchiveFile& file, const fs::path& destDir,
+		const bool overwriteExisting, const bool throwIfRemoved, const bool throwIfDuplicated) const;
+
 public:
+	explicit BloatArchive() noexcept;
 	uint8_t GetVersion() const noexcept;
 
-	std::shared_ptr<Scrambler> GetScrambler() const noexcept;
+	uint64_t GetScrambledSize() const;
+	uint64_t GetUnscrambledSize() const;
+
+	const std::shared_ptr<Scrambler>& GetScrambler() const noexcept;
 	void SetScrambler(const std::shared_ptr<Scrambler>& scrambler) noexcept;
 
-	std::vector<ArchiveFile> GetFiles() const noexcept;
-	uint64_t GetChecksum();
-
-	BloatArchive() noexcept;
+	// Some homebrewed hash accumulator function or something. We'll call it the glorious BLOATSUM (tm).
+	uint64_t GetChecksum() const;
 
 	static BloatArchive Open(const fs::path& archivePath);
 
-	void AddFile(const fs::path& filePath, const fs::path& basePath);
-	void AddFilesFromDirectory(const fs::path& dirPath, const bool recursive);
+	// Adds a file to the archive.
+	void AddFile(const fs::path& filePath, const fs::path& relativePath, const bool overwriteExisting);
 
-	void Extract(const fs::path& destDir, const bool overwriteExistingFiles);
-	void Save(const fs::path& destPath);
+	// Adds a file to the root of the archive.
+	void AddFile(const fs::path& filePath, const bool overwriteExisting);
+
+	void AddDirectory(const fs::path& dirPath, const bool recursive, const bool overwriteExisting);
+	void RemoveFile(const fs::path& filePath);
+
+#undef RemoveDirectory  // TODO
+
+	void RemoveDirectory(const fs::path& filePath);
+
+	const std::vector<ArchiveFile>& GetAllFiles() const noexcept;
+	const ArchiveFile& GetFile(const fs::path& filePath) const;
+
+	bool DoesFileExist(const fs::path& filePath) const noexcept;
+
+	// Extracts all files to the destination directory.
+	void Extract(const fs::path& destDir, const bool overwriteExistingFiles) const;
+
+	// Extracts the specified file to the destination directory.
+	void Extract(const fs::path& filePath, const fs::path& destDir, const bool overwriteExisting, const bool throwIfDuplicated) const;
+
+	void Save(const fs::path& destPath, const bool overwrite) const;
 };
