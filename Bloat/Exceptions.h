773dd14ba44c5141f7d1f47ae2e9edcce5707ce9
Wirmaple73 <wirmaple73@gmail.com>
2025-12-06 01:07:31 +0330

    Remove InvalidObfuscatorKeyException.

--- Bloat/Exceptions.h
+++ Bloat/Exceptions.h
@@ -36,10 +36,8 @@ public:
     inline explicit InvalidOperationException(const std::string& message) noexcept : std::runtime_error(message) {}
 };
 
-class InvalidObfuscatorKeyException : public std::runtime_error
 {
 public:
-    inline explicit InvalidObfuscatorKeyException(const std::string& message) noexcept : std::runtime_error(message) {}
 };
 
 class InvalidArchiveException : public std::runtime_error
