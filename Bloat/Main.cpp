d94af5157f92463f3065291daac151ff7c201aa1
Wirmaple73 <wirmaple73@gmail.com>
2025-12-06 01:06:51 +0330

    Update Main.cpp

--- Bloat/Main.cpp
+++ Bloat/Main.cpp
@@ -10,6 +10,10 @@ namespace fs = std::filesystem;
 using Operation = CmdArgsParser::Operation;
 using ExitCode  = CmdArgsParser::ExitCode;
 
+#if _WIN32
+extern "C" __declspec(dllimport) int __stdcall SetConsoleTitleW(const wchar_t* lpConsoleTitle);
+#endif
+
 static inline int GetExitCode(const ExitCode exitCode, const bool pause) noexcept
 {
 	if (pause)
@@ -21,19 +25,29 @@ static inline int GetExitCode(const ExitCode exitCode, const bool pause) noexcep
 	return static_cast<int>(exitCode);
 }
 
+static inline ArchiveManipulator CreateArchiveManipulator(const CmdArgsParser& parser)
+{
+	switch (parser.GetOperation())
+	{
+		case Operation::Version:
+		case Operation::Help:
+			return ArchiveManipulator{};
+
+		default:
+			return ArchiveManipulator{ parser.GetArchivePath(), parser.GetPassword(), parser.DoChecksumVerification() };
+	}
+}
+
 static inline std::shared_ptr<Scrambler> CreateScrambler(const CmdArgsParser& parser)
 {
 	auto scrambler = Scrambler::Create(
 		parser.GetBloatMultiplier(), ObfuscatorFactory::Create(static_cast<ObfuscatorId>(parser.GetObfuscatorId()))
 	);
 
-	if (scrambler->GetObfuscator()->SupportsKey())
-	{
-		const uint64_t key = parser.GetObfuscatorKey();
+	const uint64_t key = parser.GetObfuscatorKey();
 
-		if (key != 0ui64)
-			scrambler->GetObfuscator()->SetKey(key);
-	}
+	if (key != 0ui64)
+		scrambler->GetObfuscator()->SetKey(key);
 
 	return scrambler;
 }
@@ -42,20 +56,32 @@ int main(int argc, char* argv[])
 {
 	std::ios::sync_with_stdio(false);
 
+#if _WIN32
+	SetConsoleTitleW(L"BLOAT");
+#endif
+
 	bool pause = true;
 	bool showSuccessMessage = true;
 
+	const auto& start = std::chrono::steady_clock::now();
+
 	try
 	{
-		const CmdArgsParser parser(argc, argv);
+		const CmdArgsParser& parser{ argc, argv };
 		pause = parser.IsPauseActivated();
 
-		ArchiveManipulator am(parser.GetArchivePath(), parser.GetPassword());
+		const Operation operation = parser.GetOperation();
+		const ArchiveManipulator& am = CreateArchiveManipulator(parser);
 
-		switch (parser.GetOperation())
+		switch (operation)
 		{
 			case Operation::Info:
 				am.DisplayInfo();
+				showSuccessMessage = false;
+				break;
+
+			case Operation::Verify:
+				am.VerifyIntegrity();
 				showSuccessMessage = false;
 				break;
 
@@ -102,10 +128,19 @@ int main(int argc, char* argv[])
 
 		return GetExitCode(ExitCode::MalformedArgument, pause);
 	}
+	catch (const ChecksumMismatchException& ex)
+	{
+		std::cerr << "An error occurred while trying to open the specified archive: " << ex.what() << "\n"
+			      << std::format("(archive checksum: {}, calculated checksum: {})\n",
+					  ex.GetArchiveChecksum(), ex.GetCalculatedChecksum()
+				  );
+
+		return GetExitCode(ExitCode::ChecksumMismatch, pause);
+	}
 	catch (const std::invalid_argument& ex)
 	{
-		std::cerr << "An error occurred while parsing input arguments: " << ex.what()
-			      << " One of the switches or its parameter (value) is invalid.\n"
+		std::cerr << "An error occurred while parsing input arguments: " << ex.what() << "\n"
+			      // << " One of the switches or its value is invalid.\n"
 			      << "Please enter 'bloat help' to view usage help.\n";
 
 		return GetExitCode(ExitCode::MalformedArgument, pause);
@@ -123,8 +158,10 @@ int main(int argc, char* argv[])
 	}
 	*/
 
+	const auto& end = std::chrono::steady_clock::now();
+
 	if (showSuccessMessage)
-		std::cout << "The operation has been completed successfully.\n";
+		std::cout << std::format("The operation has been completed in {:.2f} seconds.\n", std::chrono::duration<double>(end - start).count());
 
 	return GetExitCode(ExitCode::Success, pause);
 }
